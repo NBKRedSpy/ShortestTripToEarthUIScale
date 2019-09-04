8a6d84fddfb912814ec85cfe703047e97f60ec19
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2019-09-04 11:18:13 -0400

    Initial Commit

---
+++ BepInExInit.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using BepInEx.Harmony;
+//using Harmony;
+using HarmonyLib;
+
+namespace StteUiScale
+{
+    
+    [BepInPlugin("A3B7094F-0267-401A-A917-E3EDA3EF7C5C", "StteUiScale","1.0.0.0")]
+    class BepInExInit : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            var harmony = HarmonyWrapper.PatchAll("com.github.NBKRedSpy.StteUiScale");
+
+            //The patch doesn't seem to take with the Wrapper version of the patch.  
+            //  However, invoking the PatchAll on the instance does.
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ MainPatch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using BepInEx.Harmony;
+using HarmonyLib;
+
+namespace StteUiScale
+{
+    [HarmonyPatch(typeof(RST.Settings))]
+    [HarmonyPatch( "UiScale",MethodType.Getter)]
+    public class MainPatch
+    {
+
+        /// <summary>
+        /// Hard does the UIScale, which is the computed value from the settings.
+        /// The UI depends only on this property.
+        /// </summary>
+        /// <param name="__result"></param>
+        /// <returns></returns>
+        public static bool Prefix(ref float __result)
+        {
+            __result = 1.25f;
+            return false;
+        }
+    }
+}
